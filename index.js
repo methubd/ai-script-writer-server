8c45685821f85a0bc47ba085300a1701488a4b4a
T R Methu <fmethubd@gmail.com>
2023-08-19 10:34:17 +0600

    edit functionality working

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // const openai = require('openai');
 const bodyParser = require('body-parser');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const fetch = require('node-fetch');
+var jwt = require('jsonwebtoken');
 
 
 // middleware ---->
@@ -15,9 +16,21 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json())
 
-// const openaiInstance = new openai({
-//   apiKey: process.env.GPT_SECRET
-// });
+// jwt verification
+const verifyJWT = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'Unauthorized Access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.AuthSecret, (error, decoded) => {
+    if(error){
+      return res.status(403).send({error: true, message: 'Unauthorized Access'})
+    }
+    res.decoded = decoded;
+    next();
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.some2ew.mongodb.net/?retryWrites=true&w=majority`;
@@ -40,20 +53,30 @@ async function run() {
     const generatedScriptCollections = client.db('NolanAIScripts').collection('generatedScripts');
 
     /*************************
+     * JWT
+     *************************/
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.AuthSecret, {
+        expiresIn: '1hr'
+      });
+      res.send({token});
+    })
+
+    /*************************
      * Script API's Start
      *************************/
 
     app.post('/scripts', async (req, res) => {
       try {
-          const scriptPrompt = req.body;
-  
+          const scriptPrompt = req.body;  
           const response = await fetch(
               'https://api.openai.com/v1/chat/completions',
               {
                   method: 'POST',
                   body: JSON.stringify({
                       model: 'gpt-3.5-turbo',
-                      messages: [{ role: 'system', content: 'You are a script writer.' }, { role: 'user', content: scriptPrompt.synopsis + 'make this within 100 words', }]
+                      messages: [{ role: 'system', content: 'You are a script writer.' }, { role: 'user', content: scriptPrompt.synopsis + 'content genre would be' + scriptPrompt.genre + 'make this within 200 words', }]
                   }),
                   headers: {
                       'Content-Type': 'application/json',
@@ -61,6 +84,8 @@ async function run() {
                   }
               }
           );
+
+          res.send(response)
   
           if (!response.ok) {
               throw new Error(`Error response from ChatGPT API: ${response.status} ${response.statusText}`);
@@ -70,30 +95,66 @@ async function run() {
           const generatedScript = responseData.choices[0].message.content;
 
           const result = await generatedScriptCollections.insertOne({
-            prompt: scriptPrompt.title,
+            prompt: scriptPrompt.synopsis,
+            title: scriptPrompt.title,
             genre: scriptPrompt.genre,
+            email: scriptPrompt.email,
             generatedScript: generatedScript,
             createdAt: new Date()
           })
 
           res.send({result})
   
-      } catch (error) {
-          console.error('Error generating and saving script:', error);
-          res.send({ error: 'An error occurred while generating and saving script' });
-      }
-  });
+        } catch (error) {
+            console.error('Error generating and saving script:', error);
+            res.send({ error: 'An error occurred while generating and saving script' });
+        }
+    });
 
-    // Getting stored scripts
-    app.get('/scripts', async (req, res) => {
+    app.get('/scripts/:email', verifyJWT, async (req, res) => {
       try {
-        const result = await generatedScriptCollections.find().toArray();
+        const email = req.params.email;
+        const query = {email: email}
+        const result = await generatedScriptCollections.find(query).toArray();
         res.status(200).send(result);
       } catch (error) {
         console.error('Error sending scripts:', error);
         res.status(500).send({ error: 'An error occurred while generating and saving script' });
       }
     })
+
+    app.get('/specific-scripts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await generatedScriptCollections.findOne(query);
+      res.send(result)
+    })
+
+    app.put('/edit-scripts/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true}
+      const requestedChanges = req.body;
+      const updatedScript = {
+        $set: {
+          title: requestedChanges.title,
+          generatedScript: requestedChanges.generatedScript
+        }
+      }
+      const result = await generatedScriptCollections.updateOne(filter, updatedScript, options);
+      res.send(result)
+    })
+
+    app.delete('/scripts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await generatedScriptCollections.deleteOne(query);
+      res.send(result)
+    })
+
+    /*************************
+     * Script API's End
+    *************************/
 
 
     // Send a ping to confirm a successful connection
